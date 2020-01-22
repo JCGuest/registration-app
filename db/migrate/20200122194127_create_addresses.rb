c8a2e3838d016274205f521347fd03013ef126aa
JCGuest <jcguest90@gmail.com>
2020-01-22 14:31:58 -0600

    first commit

---
+++ db/migrate/20200122194127_create_addresses.rb
@@ -0,0 +1,15 @@
+class CreateAddresses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :addresses do |t|
+      t.string :street_1
+      t.string :street_2
+      t.string :city
+      t.string :state
+      t.string :address_type
+      t.string :zip_code
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
